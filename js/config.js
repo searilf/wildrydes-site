e713633b26b16e9fc530d6c3ac351ffc45a38389
searilf <56870493+searilf@users.noreply.github.com>
2025-03-12 16:35:25 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Ryr34uwyR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5mcioro8hqdgj3asm1mfos3l4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
